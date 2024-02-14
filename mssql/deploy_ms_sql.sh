5ac5345fc161bc059421e9d5f01f513efb302bd2
Witold Wierzchowski <witold.wierzchowski@cohesivegroup.com>
2024-02-14 10:20:33 +0100

    ms sql deployment with fts

---
+++ mssql/deploy_ms_sql.sh
@@ -0,0 +1,12 @@
+oc create -f project.yaml
+
+oc project mssqlfts2019
+
+oc create -f scc.yaml
+oc adm policy add-scc-to-group restrictedfsgroup system:serviceaccounts:mssql
+
+oc create secret generic mssql --from-literal=SA_PASSWORD="Sql2019isfast"
+
+oc apply -f storage.yaml
+
+oc apply -f deployment.yaml
