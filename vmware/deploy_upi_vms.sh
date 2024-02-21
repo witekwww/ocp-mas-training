4391981e520fccdb9b8242c5753daa8eb7acad3b
Witold Wierzchowski <118370754+witekwww@users.noreply.github.com>
2024-02-21 11:57:38 +0100

    Update deploy_upi_vms.sh
    typos

--- vmware/deploy_upi_vms.sh
+++ vmware/deploy_upi_vms.sh
@@ -28,11 +28,11 @@ govc vm.change -e="disk.enableUUID=1" -vm=w2
 govc vm.create -on=false -c=8 -m=32768 -disk=160GB -iso-datastore=gym-550000532v-ia4savhl-storage -iso=isos/discovery_image_upicluster.iso w3
 govc vm.change -e="disk.enableUUID=1" -vm=w3
 
-govc vm.create -on=false -c=10 -m=24567 -disk=160GB -iso-datastore=gym-550000532v-ia4savhl-storage -iso=isos/discovery_image_upicluster.iso i1
+govc vm.create -on=false -c=10 -m=24576 -disk=160GB -iso-datastore=gym-550000532v-ia4savhl-storage -iso=isos/discovery_image_upicluster.iso i1
 govc vm.change -e="disk.enableUUID=1" -vm=i1
 
-govc vm.create -on=false -c=10 -m=24567 -disk=160GB -iso-datastore=gym-550000532v-ia4savhl-storage -iso=isos/discovery_image_upicluster.iso i2
+govc vm.create -on=false -c=10 -m=24576 -disk=160GB -iso-datastore=gym-550000532v-ia4savhl-storage -iso=isos/discovery_image_upicluster.iso i2
 govc vm.change -e="disk.enableUUID=1" -vm=i2
 
-govc vm.create -on=false -c=10 -m=24567 -disk=160GB -iso-datastore=gym-550000532v-ia4savhl-storage -iso=isos/discovery_image_upicluster.iso i3
+govc vm.create -on=false -c=10 -m=24576 -disk=160GB -iso-datastore=gym-550000532v-ia4savhl-storage -iso=isos/discovery_image_upicluster.iso i3
 govc vm.change -e="disk.enableUUID=1" -vm=i3
